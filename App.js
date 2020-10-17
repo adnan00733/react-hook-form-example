340f6b807c4b56bb3fe283e6a8a97158d94fa199
Adnan Ahmad <adnan00733@yahoo.com>
2020-10-18 00:28:17 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useForm } from 'react-hook-form'
+
+
+const onSubmit = data => {
+  console.log(data);
+}
+function App() {
+  const { register , handleSubmit , errros} = useForm();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" name="email" ref={register} />
+          <input type="checkbox" name="contact[]" id="contact-one" value="1" ref={register} /> <label htmlFor="contact-one">One</label>
+          <input type="checkbox" name="contact[]" id="contact-two" value="2" ref={register} /> <label htmlFor="contact-two">Two</label>
+          <button type="submit">Submit</button>
+        </form>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
